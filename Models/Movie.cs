3fb2699cfd752e32f0316ac09b300d48feeb9300
thuringia <robert-waw@fb4.de>
2012-02-03 19:16:18 +0100

    Added two models (movie and genre)

---
+++ Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAP_Assignment.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
