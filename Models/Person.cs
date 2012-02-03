2b624a689d6e19a055fb2339b0c1eae7e74e7ef0
thuringia <robert-waw@fb4.de>
2012-02-03 19:56:16 +0100

    Added the Person model for members of a movie's cast (based on themoviedb.org)

---
+++ Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAP_Assignment.Models
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string character { get; set; }
+        public string job { get; set; }
+        public int id { get; set; }
+        // public ??? thumb { get; set; }
+        public string department { get; set; }
+        public string url { get; set; }
+        public string order { get; set; }
+        public string cast_id { get; set; }
+    }
+}
