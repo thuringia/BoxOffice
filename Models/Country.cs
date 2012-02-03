f158f1474a7d3f09c08b12480152b3358c073d8e
thuringia <robert-waw@fb4.de>
2012-02-03 21:08:51 +0100

    Modified the Movie model and added Country as another dependency

---
+++ Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAP_Assignment.Models
+{
+    public class Country
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+        public string url { get; set; }
+    }
+}
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,7 +7,30 @@ namespace WAP_Assignment.Models
 {
     public class Movie
     {
-        public string Title { get; set; }
-        public Genre Genre { get; set; }
+        public bool adult { get; set; }
+        public bool translated { get; set; }
+        public string language { get; set; }
+        public string original_name { get; set; }
+        public string name { get; set; }
+        public string alternative_name { get; set; }
+        public string type { get; set; }
+        public int id { get; set; }
+        public string imdb_id { get; set; }
+        public string url { get; set; }
+        public string overview { get; set; }
+        public int votes { get; set; }
+        public float rating { get; set; }
+        public bool rating_by_moviedb { get; set; }
+        public bool language { get; set; }
+        public string tagline { get; set; }
+        public string certification { get; set; }
+        public DateTime released { get; set; }
+        public string homepage { get; set; }
+        public string trailer { get; set; }
+        public List<Category> categories { get; set; }
+        public List<Studio> studios { get; set; }
+        public List<Country> countries { get; set; }
+        public List<Image> images { get; set; }
+        public List<Person> cast { get; set; }
     }
 }
