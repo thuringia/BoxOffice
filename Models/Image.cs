af2b9d44661714b65655b61f0148db6f4cb8f299
thuringia <robert-waw@fb4.de>
2012-02-03 19:58:53 +0100

    Added the Image model (based on themoviedb.org)

---
+++ Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAP_Assignment.Models
+{
+    public class Image
+    {
+        public string type { get; set; }
+        public string url { get; set; }
+        public string size { get; set; }
+        public string id { get; set; }
+    }
+}
