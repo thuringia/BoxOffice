04162b6146b5ccf57bc33bda2fae2c4735fb6ecd
thuringia <robert-waw@fb4.de>
2012-02-03 21:19:08 +0100

    Set up entity framework

---
+++ Models/AssignmentEntities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WAP_Assignment.Models : DbContext
+{
+    public class AssignmentEntities
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Studio> Studios { get; set; }
+    }
+}
