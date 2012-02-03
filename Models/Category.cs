7ec39db3ced149336b4f289649db1d53ac9656f0
thuringia <robert-waw@fb4.de>
2012-02-03 20:03:12 +0100

    Added models for categories and studios, based on themoviedb.org

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAP_Assignment.Models
+{
+    public class Category
+    {
+        public string type { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ Models/Studio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAP_Assignment.Models
+{
+    public class Studio
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public int id { get; set; }
+    }
+}
