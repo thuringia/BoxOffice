24c7f261440132401106a627b4780b538f81e00c
Robert Wawrzyniak <robert-waw@fb4.de>
2012-02-14 14:31:38 +0100

    Added a Model for DVDs

---
+++ Models/DVD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAP_Assignment.Models
+{
+    /// <summary>
+    /// A Model class describing a physical DVD, that can be sent to a customer
+    /// </summary>
+    public class DVD
+    {
+        public int DvdID { get; set; }
+        public int MovieID { get; set; }
+        public IEnumerable<Rental> Rentals { get; set; }
+        public string State { get; set; }
+    }
+}
