72cce2d7a99289877e1751eb7e392ca3459cebaa
Robert Wawrzyniak <robert-waw@fb4.de>
2012-02-14 14:46:02 +0100

    Added a stub for the User Model

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAP_Assignment.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+    }
+}
