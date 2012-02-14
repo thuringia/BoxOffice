f454b6ec6b82ee64e8be694a8aa304fffd3cbeb8
Robert Wawrzyniak <robert-waw@fb4.de>
2012-02-14 15:18:21 +0100

    added the Adress Model

---
+++ Models/Adress.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace WAP_Assignment.Models
+{
+    public class Adress
+    {
+        public int AdressID { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+
+        [Required]
+        [MaxLength(7)]
+        public string Number { get; set; }
+
+        [Required]
+        [MaxLength(8)]
+        public string Zip { get; set; }
+
+        [Required]
+        public string City { get; set; }
+    }
+}
