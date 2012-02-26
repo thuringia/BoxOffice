9c0661db44eabb2cdc2114f5294e1f28a7f713dd
Robert Wawrzyniak <robert-waw@fb4.de>
2012-02-26 21:08:26 +0100

    Moved the Bootstrap and fixed some errors.

--- Models/Bootstrap.cs
+++ Models/Bootstrap.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
-using BoxOffice.Models;
 
-namespace BoxOffice.Bootstrap
+namespace BoxOffice.Models
 {
-    public class Bootstrap : DropCreateDatabaseIfModelChanges<BoxOfficeContext>
+    public class Bootstrap : DropCreateDatabaseAlways<BoxOfficeContext>
     {
         protected override void Seed(BoxOfficeContext context)
         {
@@ -36,20 +35,10 @@ namespace BoxOffice.Bootstrap
             context.Movies.Add(movie);
             context.SaveChanges();
 
-            var rating = new Rating
-            {
-                RatingID = 0,
-                MovieID = 187,
-                UserID = 0,
-                Date = DateTime.Now
-            };
-            context.Ratings.Add(rating);
-            context.SaveChanges();
-
             var categories = new List<Category> {
-                new Category { CategoryID = 80, Name = "Crime", Url = "http://themoviedb.org/genre/crime", Type = "genre", MovieID = 187 },
-                new Category { CategoryID = 18, MovieID = 187, Type = "genre", Url = "http://themoviedb.org/genre/drama", Name = "Drama" },
-                new Category { CategoryID = 53, Name = "Thriller", Url = "http://themoviedb.org/genre/thriller", Type = "genre", MovieID=187 }  
+                new Category { CategoryID = 80, Name = "Crime", Url = "http://themoviedb.org/genre/crime", Type = "genre" },
+                new Category { CategoryID = 18, Type = "genre", Url = "http://themoviedb.org/genre/drama", Name = "Drama" },
+                new Category { CategoryID = 53, Name = "Thriller", Url = "http://themoviedb.org/genre/thriller", Type = "genre" }  
             };
             categories.ForEach(s => context.Categories.Add(s));
             context.SaveChanges();
@@ -77,8 +66,7 @@ namespace BoxOffice.Bootstrap
                 Type = "poster",
                 Url = "http://hwcdn.themoviedb.org/posters/68c/4bc904e9017a3c57fe00168c/sin-city-original.jpg",
                 Size = "original",
-                ImageID = "4bc904e9017a3c57fe00168c",
-                MovieID = 178
+                ImageID = "4bc904e9017a3c57fe00168c"
             };
             context.Images.Add(image);
             context.SaveChanges();
@@ -96,61 +84,65 @@ namespace BoxOffice.Bootstrap
             context.Persons.Add(person);
             context.SaveChanges();
 
+            //var adresses = new List<Adress>
+            //{
+            //    new Adress
+            //    {
+            //        AdressID = 1,
+            //        Street = "Foo Street",
+            //        Number = "1",
+            //        Zip = "12345",
+            //        City = "Foocity"
+            //    },
+            //    new Adress
+            //    {
+            //        AdressID = 2,
+            //        Street = "Bar Road",
+            //        Number = "1",
+            //        Zip = "54321",
+            //        City = "Bar Town"
+            //    }
+            //};
+            //adresses.ForEach(s => context.Adresses.Add(s));
+            //context.SaveChanges();
+
             var users = new List<User>
             {
                 new User 
                 {
-                    UserID = 0,
+                    UserID = 1,
                     Username = "foo",
                     Email = "foo@bar.com",
                     DateOfBirth = DateTime.Parse("1988-02-13"),
                     Password = "foobar",
-                    AdressID = 0,
+                    Street = "Foo",
+                    Number = "1",
+                    Zip = "12345",
+                    City = "Foo",
                     isAdmin = false
                 },
                 new User
                 {
-                    UserID = 1,
+                    UserID = 2,
                     Username = "bar",
                     Email = "bar@foo.com",
                     DateOfBirth = DateTime.Parse("1988-02-13"),
                     Password = "foobar",
-                    AdressID = 1,
+                    Street = "Foo",
+                    Number = "1",
+                    Zip = "12345",
+                    City = "Foo",
                     isAdmin = false
                 }
             };
             users.ForEach(s => context.Users.Add(s));
             context.SaveChanges();
 
-            var adresses = new List<Adress>
-            {
-                new Adress
-                {
-                    UserID = 0,
-                    AdressID = 0,
-                    Street = "Foo Street",
-                    Number = "1",
-                    Zip = "12345",
-                    City = "Foocity"
-                },
-                new Adress
-                {
-                    UserID = 1,
-                    AdressID = 1,
-                    Street = "Bar Road",
-                    Number = "1",
-                    Zip = "54321",
-                    City = "Bar Town"
-                }
-            };
-            adresses.ForEach(s => context.Adresses.Add(s));
-            context.SaveChanges();
-
             var comment = new Comment
             {
-                CommentID = 0,
+                CommentID = 1,
                 MovieID = 187,
-                UserID = 0,
+                UserID = 1,
                 Message = "Awesome movie!!!",
                 Date = DateTime.Now
             };
@@ -158,7 +150,7 @@ namespace BoxOffice.Bootstrap
             context.SaveChanges();
 
             var dvds = new List<DVD>{};
-            for (int i = 0; i < 5; i++)
+            for (int i = 1; i < 6; i++)
             {
                 var dvd = new DVD
                 {
@@ -173,9 +165,9 @@ namespace BoxOffice.Bootstrap
 
             var rental = new Rental
             {
-                RentalID = 0,
-                DvdID = 0,
-                UserID = 0,
+                RentalID = 1,
+                DvdID = 1,
+                UserID = 1,
                 DateOfRental = DateTime.Now,
                 DueDate = DateTime.Now
             };
@@ -184,14 +176,24 @@ namespace BoxOffice.Bootstrap
 
             var msg = new Message
             {
-                MessageID = 0,
-                FromUserID = 0,
-                ToUserID = 1,
+                MessageID = 1,
+                FromUserID = 1,
+                ToUserID = 2,
                 Date = DateTime.Now,
                 Text = "You suck!",
                 toAll = false
             };
             context.Messages.Add(msg);
+            context.SaveChanges();
+
+            var rating = new Rating
+            {
+                RatingID = 1,
+                MovieID = 187,
+                UserID = 1,
+                Date = DateTime.Now
+            };
+            context.Ratings.Add(rating);
             context.SaveChanges();
 
             base.Seed(context);
