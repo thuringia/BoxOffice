610340782493db8688d4a34c10002e526eb06582
Robert Wawrzyniak <robert-waw@fb4.de>
2012-02-28 12:25:22 +0100

    Modified the Account Models to be in BoxOffice's namespace

--- Models/AccountModels.cs
+++ Models/AccountModels.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 using System.Web.Mvc;
 using System.Web.Security;
 
-namespace MvcApplication1.Models
+namespace BoxOffice.Models
 {
 
     public class ChangePasswordModel
