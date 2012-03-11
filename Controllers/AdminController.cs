6a482e18d831506adef715b01c9964a2e4bdbc31
Robert Wawrzyniak <robert-waw@fb4.de>
2012-03-11 18:26:21 +0100

    Commented out the role check to develop more quickly.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace BoxOffice.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         //
