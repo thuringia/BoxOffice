ee3ea868c45b7c5eb7e92930c0f2bd1a5c5d3dcc
Robert Wawrzyniak <robert-waw@fb4.de>
2012-03-11 20:40:17 +0100

    Added a controller action to return all movies to its view.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BoxOffice.Models;
 
 namespace BoxOffice.Controllers
 {
     //[Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private BoxOfficeContext db = new BoxOfficeContext();
+
         //
         // GET: /Admin/
 
         public ActionResult Index()
         {
             return View();
+        }
+
+        //
+        // GET: /Admin/Movies
+
+        public ActionResult Movies()
+        {
+            return View(db.Movies.ToList());
         }
 
     }
