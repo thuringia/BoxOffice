427fbcf5bdc6e81c31877493ae089c3a11f5be86
Robert Wawrzyniak <robert-waw@fb4.de>
2012-03-15 18:33:07 +0100

    added comments for documentation

--- Models/Rating.cs
+++ Models/Rating.cs
@@ -10,16 +10,35 @@ namespace BoxOffice.Models
 {
     public class Rating
     {
+        /// <summary>
+        /// This rating's unique ID
+        /// </summary>
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int RatingID { get; set; }
 
+        /// <summary>
+        /// The MovieID FK of the rented movie
+        /// </summary>
         public int MovieID { get; set; }
+
+        /// <summary>
+        /// The navigation property for the rented movie
+        /// </summary>
         public virtual Movie Movie { get; set; }
 
+        /// <summary>
+        /// The renter's UserID as FK
+        /// </summary>
         public int UserID { get; set; }
+
+        /// <summary>
+        /// The renter's User object as navigation property
+        /// </summary>
         public virtual User User { get; set; }
 
-
+        /// <summary>
+        /// The date the rental was issued
+        /// </summary>
         public DateTime DateRented { get; set; }
     }
 }
