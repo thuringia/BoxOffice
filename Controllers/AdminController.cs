e970e38c7cb63d01360673d583ec71b9420382a3
Robert Wawrzyniak <robert-waw@fb4.de>
2012-03-16 23:56:12 +0100

    added the controller code for the admin dashboard

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -17,7 +17,34 @@ namespace BoxOffice.Controllers
         // GET: /Admin/
 
         public ActionResult Index()
-        {
+        {                    
+            // add movie of the week to ViewData, so we can display it
+            ViewData["movieOfTheWeek"] = from m in db.Movies
+                                         where m.MovieOfTheWeek == true
+                                         select m;
+
+            // create a dictionary for our rental charts
+            var rentalCharts = new Dictionary<Movie, int>();
+
+            // loop over all movies to get their rental count
+            foreach (var item in db.Movies.ToList())
+            {
+                int rentalCount = 0;
+                item.DVDs.ToList().ForEach(s => rentalCount += s.Rentals.Count());
+                rentalCharts.Add(item, rentalCount);
+            }
+
+            // sort the charts and get the 10 most rented
+            ViewData["hotMovies"] = (from m in rentalCharts
+                                    orderby m.Value
+                                    select m).Take(10);
+
+            // get all comments that need administrative action
+
+            ViewData["flaggedComments"] = from c in db.Comments
+                                          where (c.Flag == 5) || (c.Flag > 5)
+                                          orderby c.Flag
+                                          select c;
             return View();
         }
 
