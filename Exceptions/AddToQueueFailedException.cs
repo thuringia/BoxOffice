0ad716598a4ec73d29a648b64df3179a35b33dd9
Robert Wawrzyniak <robert-waw@fb4.de>
2012-04-20 00:55:35 +0100

    added a custom exception for adding a movie to a user's queue

---
+++ Exceptions/AddToQueueFailedException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOffice.Controllers
+{
+    class AddToQueueFailedException : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "adding to queue failed";
+            }
+        }
+    }
+}
