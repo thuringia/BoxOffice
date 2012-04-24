7ff20f9876d9c9f1474a5eb42dd423a46bc8bcc9
Robert Wawrzyniak <robert-waw@fb4.de>
2012-04-24 05:05:46 +0100

    created RequestFailedException

---
+++ BoxOffice/Exceptions/RequestFailedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOffice.Exceptions
+{
+    [Serializable]
+    class RequestFailedException : Exception
+    {
+        override string Message
+        {
+            get
+            {
+                return "request failed";
+            }
+        }
+    }
+}
