e8a975f08b0bb566fc22760f906efd5c7779d96c
Robert Wawrzyniak <robert-waw@fb4.de>
2012-04-29 18:29:45 +0100

    marked as only accessible for Roles = "user"

--- BoxOffice/Controllers/UsersController.cs
+++ BoxOffice/Controllers/UsersController.cs
@@ -10,6 +10,7 @@ using System.Web.Security;
 
 namespace BoxOffice.Controllers
 { 
+    [Authorize(Roles = "User")]
     public class UsersController : Controller
     {
         private BoxOfficeContext db = new BoxOfficeContext();
